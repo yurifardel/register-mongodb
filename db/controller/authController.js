a6e189956ece22be677d824d3a3ec766b800c23c
yurifardel <yrnascimento@outlook.com>
2021-04-27 22:57:29 -0300

    mongodb data return problem solved

--- db/controller/authController.js
+++ db/controller/authController.js
@@ -1,15 +1,22 @@
 const express = require("express");
 
-const User = require("../models/user");
+const User = require("../models/User");
 
 const router = express.Router();
 
 router.post("/register", async (request, response) => {
-  console.log(request.body);
+  const { email } = request.body;
+
   try {
+    if (await User.findOne({ email }))
+      return response.status(400)({ error: "user already exists" });
+
     const user = User.create(request.body);
 
-    return response.send({ user });
+    user.password = undefined;
+    user.then((Users) => {
+      return response.send({ Users });
+    });
   } catch (err) {
     return response.status(400).send({ error: "Registration failed" });
   }
--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("../db");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -7,13 +8,25 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    require: true,
+    unique: true,
     lowercase: true,
   },
   password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  createdAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
