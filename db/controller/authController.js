7b2647bf7865512a9d3382e5141ed60e85e5eabb
yurifardel <yrnascimento@outlook.com>
2021-04-28 22:17:19 -0300

    authenticate

--- db/controller/authController.js
+++ db/controller/authController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
@@ -20,6 +21,24 @@ router.post("/register", async (request, response) => {
   } catch (err) {
     return response.status(400).send({ error: "Registration failed" });
   }
+});
+
+router.post("/authenticate", async (request, response) => {
+  const { email, password } = request.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return response.status(400).send({ error: "user not found" });
+  }
+
+  console.log(user.password, user.password);
+
+  if (!(await bcrypt.compare(user.password))) {
+    return response.status(400).send({ error: "invalid password" });
+  }
+
+  return response.send({ user });
 });
 
 module.exports = (app) => app.use("/auth", router);
