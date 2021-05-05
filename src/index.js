0a70807267d019f5a2e3ada479484140eab13d74
yurifardel <yrnascimento@outlook.com>
2021-05-04 21:37:28 -0300

    created single route path for controllers

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require("../db/controller/authController")(app);
-require("../db/controller/projectController")(app);
+// require("../db/controller/authController")(app);
+// require("../db/controller/projectController")(app);
+
+require("../db/controller/index")(app);
 
 app.listen(3333, () => {
   console.log("express listening at port 3333");
