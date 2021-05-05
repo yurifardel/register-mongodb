03ebb5408544a103309d6acf1414b15111d0d837
yurifardel <yrnascimento@outlook.com>
2021-05-05 16:25:31 -0300

    .

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,16 +1,21 @@
 const nodemailer = require("nodemailer");
-const jsonConfigMailer = require("../../lib/config/mailer.json");
+const jsonConfigMailer = require("../../lib/config/mail.json");
 const hbs = require("nodemailer-express-handlebars");
 const path = require("path");
 
-const { host, pass, port, user } = jsonConfigMailer;
+const {
+  host,
+  pass,
+  port,
+  user
+} = jsonConfigMailer;
 
 const transport = nodemailer.createTransport({
-  host: host,
-  port: port,
+  host,
+  port,
   auth: {
-    user: user,
-    pass: pass,
+    user,
+    pass,
   },
 });
 
@@ -18,7 +23,7 @@ transport.use(
   "compile",
   hbs({
     viewEngine: "handlebars",
-    viewPath: path.resolve("./src/resources/mail"),
+    viewPath: path.resolve("./src/resources/mail/"),
     extName: ".html",
   })
 );
