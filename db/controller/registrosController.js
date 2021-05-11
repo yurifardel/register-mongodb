1a17573ce237d3aea42307e765f89b2bc23a4cf5
yurifardel <yrnascimento@outlook.com>
2021-05-11 00:58:18 -0300

    register controller update

--- db/controller/registrosController.js
+++ db/controller/registrosController.js
@@ -4,17 +4,41 @@ const router = express.Router();
 const modelUser = require("../models/User");
 const modelRegistros = require("../models/Registros");
 
-router.post("/", async (req, res) => {
-  const user = modelUser.find().limit(3);
+router.post("/create", async (req, res) => {
+  const { page = 1 } = req.query;
 
-  const userPromiseReturn = user.then(async (user) => {
-    await modelRegistros.update({
-      registros: { user },
-    });
-    return res.status(200).send({ success: "ok" });
+  await modelRegistros.create({
+    pagina: page,
+    registros: [],
   });
 
-  return userPromiseReturn;
+  res.status(200).send();
 });
 
-module.exports = (app) => app.use("/registros", router);
+router.post("/update", async (req, res) => {
+  try {
+    const user = modelUser.find();
+
+    const userPromiseReturn = user.then(async (user) => {
+      const ids = user.map((id) => {
+        console.log(id.id);
+        return id.id;
+      });
+
+      console.log(ids.length);
+
+      await modelRegistros.update({
+        registros: { user },
+      });
+
+      return res.status(200).send({ success: "ok" });
+    });
+
+    // return userPromiseReturn;
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(TypeError("not found"));
+  }
+});
+
+module.exports = (app) => app.use("/register", router);
--- db/models/registros.js
+++ db/models/registros.js
@@ -4,6 +4,10 @@ const RegistroSchema = new mongoose.Schema({
   registros: {
     type: [],
   },
+  pagina: {
+    type: Number,
+    requered: true,
+  },
 });
 
 module.exports = mongoose.model("Registro", RegistroSchema);
