46c51558ee163ce27dda794acb777fde95e504f0
yurifardel <yrnascimento@outlook.com>
2021-06-17 23:37:18 -0300

    created project controller

--- db/controller/projectController.js
+++ db/controller/projectController.js
@@ -13,4 +13,36 @@ router.get("/", (request, response) => {
   }
 });
 
+router.get("/:projectId", async (request, response) => {
+  try {
+    return response.send({ user: request.userId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/", async (request, response) => {
+  try {
+    return response.send({ user: request.userId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/:projectId", async (request, repsonse) => {
+  try {
+    return response.send({ user: request.userId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:projectId", async (request, repsonse) => {
+  try {
+    return response.send({ user: request.userId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = (app) => app.use("/project", router);
